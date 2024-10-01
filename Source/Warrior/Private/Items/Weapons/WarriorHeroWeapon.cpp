d855147f36d23a89a57581f85c26427bec0537d8
jack <swang81@icloud.com>
2024-10-02 00:33:33 +0800

    21

---
+++ Source/Warrior/Private/Items/Weapons/WarriorHeroWeapon.cpp
@@ -0,0 +1,25 @@
+﻿// 
+
+
+#include "Items/Weapons/WarriorHeroWeapon.h"
+
+
+// Sets default values
+AWarriorHeroWeapon::AWarriorHeroWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AWarriorHeroWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWarriorHeroWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
